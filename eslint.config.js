717cb4577f483c1a6d0c586499b85470ea206227
jucegt <julio2201@gmail.com>
2023-04-27 00:43:46 -0600

    chore: added eslint config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'quotes': ['error', 'single'],
+    'no-undef': ['error'],
+    'semi': ['error', 'always'],
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+};
