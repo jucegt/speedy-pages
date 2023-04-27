9769b7d76502f74416362fd32f47955c7facf134
jucegt <julio2201@gmail.com>
2023-04-27 00:44:00 -0600

    chore: added prettier config

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  overrides: [
+    {
+      files: ['**/*.css', '**/*.scss', '**/*.html'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
