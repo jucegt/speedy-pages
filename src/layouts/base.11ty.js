ea7a406a8cdb97a8f31bb0bc7a8fe9b2cda4b6b5
jucegt <julio2201@gmail.com>
2023-06-19 20:31:05 -0600

    feat: added inline css utility

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -1,8 +1,11 @@
 // Components
 const header = require('../components/header');
 
+// Utilities
+const inlineCss = require('../utilities/css');
+
 const base = {
-  render: ({ config, content }) => {
+  render: async ({ config, content }) => {
     return /* html */ `
     <!doctype html>
     <html ⚡ lang="${config.language}" class="${config.dark ? 'dark' : ''}">
@@ -20,7 +23,7 @@ const base = {
         <!-- AMP -->
         <script async src="https://cdn.ampproject.org/v0.js"></script>
         <!-- STYLES -->
-        <style amp-custom>/* CSS HERE */</style>
+        <style amp-custom>${await inlineCss('style.css')}</style>
         <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
         <!-- CANONICAL -->
         <link rel="canonical" href="." />
---
+++ src/utilities/css.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs').promises;
+const path = require('node:path');
+
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const inlineCss = async (file) => {
+  const styles = await fs
+    .readFile(path.resolve(__dirname, `../assets/css/${file}`))
+    .then(async (css) => {
+      const minified = await postcss([autoprefixer, cssnano]).process(css, {
+        from: '',
+        to: '',
+      });
+      return minified.css;
+    });
+
+  return styles;
+};
+
+module.exports = inlineCss;
