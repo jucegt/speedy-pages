0be0da65f4eabb8eeafad52f63b9147a15e6ba68
jucegt <julio2201@gmail.com>
2023-06-26 21:28:39 -0600

    feat: added amp-script

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -24,9 +24,11 @@ const base = {
         <link rel="preload" href="/static/fonts/lato-regular.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="/static/fonts/lato-bold.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
+        <link rel="preload" as="script" href="https://cdn.ampproject.org/v0/amp-script-0.1.js">
         <!-- AMP -->
         <script async src="https://cdn.ampproject.org/v0.js"></script>
         <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
+        <script async custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>
         <!-- STYLES -->
         <style amp-custom>${await inlineCss('style.css')}</style>
         <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
