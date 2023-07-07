fca25a6379a68fb39fb5b8bf6ff2333489b5e11e
jucegt <julio2201@gmail.com>
2023-07-06 22:24:30 -0600

    chore: reset to the new design

--- src/components/mask.js
+++ src/components/mask.js
@@ -1,6 +1,5 @@
 const mask = () => {
   return /* html */ `
-    <div role="button" aria-label="Cerrar" class="mask" [class]="menu || search ? 'mask mask--show' : 'mask'" on="tap:AMP.setState({ menu: false, search: false })"></div>
   `;
 };
 
--- src/components/state.js
+++ src/components/state.js
@@ -1,13 +1,5 @@
 const state = () => {
   return /* html */ `
-    <amp-state id="global">
-      <script type="application/json">
-        {
-          "menu": false,
-          "search": false
-        }
-      </script>
-    </amp-state>
   `;
 };
 
