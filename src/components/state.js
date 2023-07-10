14a2d5c0d03de841630b937ed5fbf1dbd8ba3020
jucegt <julio2201@gmail.com>
2023-07-09 22:36:49 -0600

    feat: added global state

--- src/components/state.js
+++ src/components/state.js
@@ -1,5 +1,13 @@
 const state = () => {
   return /* html */ `
+    <amp-state id="global">
+      <script type="application/json">
+        {
+          "menu": false,
+          "search": false
+        }
+      </script>
+    </amp-state>
   `;
 };
 
