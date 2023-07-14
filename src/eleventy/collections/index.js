d7a5bc2e5d3c27407e20b60aefb556eeaebff516
jucegt <julio2201@gmail.com>
2023-07-13 20:49:38 -0600

    feat: fixed collections

--- src/eleventy/collections/index.js
+++ src/eleventy/collections/index.js
@@ -1,21 +1,23 @@
 const posts = require('./posts');
 const categories = require('./categories');
-const categoryByName = require('./category-by-name');
-const categoriesList = require('./categories-list');
+const pages = require('./pages');
 const pagedPosts = require('./paged-posts');
 const pagedCategories = require('./paged-categories');
-const pages = require('./pages');
+const categoryByName = require('./category-by-name');
+const categoriesList = require('./categories-list');
 const pagesList = require('./pages-list');
+const categoriesMenu = require('./categories-menu');
 
 const collections = function (config) {
   config.addCollection('posts', posts);
   config.addCollection('categories', categories);
-  config.addCollection('categoryByName', categoryByName);
-  config.addCollection('categoriesList', categoriesList(config));
+  config.addCollection('pages', pages);
   config.addCollection('pagedPosts', pagedPosts);
   config.addCollection('pagedCategories', pagedCategories);
-  config.addCollection('pages', pages);
+  config.addCollection('categoryByName', categoryByName);
+  config.addCollection('categoriesList', categoriesList(config));
   config.addCollection('pagesList', pagesList);
+  config.addCollection('categoriesMenu', categoriesMenu(config));
 };
 
 module.exports = collections;
