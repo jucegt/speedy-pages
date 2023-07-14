927442136282d0574a3d3e0f20577daf514c5114
jucegt <julio2201@gmail.com>
2023-07-13 20:29:45 -0600

    feat: added pagesList and added categoriesList collections

--- src/eleventy/collections/index.js
+++ src/eleventy/collections/index.js
@@ -5,15 +5,17 @@ const categoriesList = require('./categories-list');
 const pagedPosts = require('./paged-posts');
 const pagedCategories = require('./paged-categories');
 const pages = require('./pages');
+const pagesList = require('./pages-list');
 
 const collections = function (config) {
   config.addCollection('posts', posts);
   config.addCollection('categories', categories);
   config.addCollection('categoryByName', categoryByName);
-  config.addCollection('categoriesList', categoriesList);
+  config.addCollection('categoriesList', categoriesList(config));
   config.addCollection('pagedPosts', pagedPosts);
   config.addCollection('pagedCategories', pagedCategories);
   config.addCollection('pages', pages);
+  config.addCollection('pagesList', pagesList);
 };
 
 module.exports = collections;
