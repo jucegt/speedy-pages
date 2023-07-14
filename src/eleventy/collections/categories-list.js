68cdff01c591e97d0d677ec5911e11c1f8e9c020
jucegt <julio2201@gmail.com>
2023-07-13 20:29:08 -0600

    feat: fixed categoriesList collection

--- src/eleventy/collections/categories-list.js
+++ src/eleventy/collections/categories-list.js
@@ -1,23 +1,25 @@
-const categoriesList = function (collection) {
-  const posts = require('./posts')(collection);
-  const categoriesNames = new Set();
-  const categoriesList = [];
+const categoriesList = function (config) {
+  return function (collection) {
+    const posts = require('./posts')(collection);
+    const categoriesNames = new Set();
+    const categoriesList = [];
 
-  posts.map((post) => {
-    if ('category' in post.data) {
-      categoriesNames.add(post.data.category);
-    }
-  });
-
-  [...categoriesNames].map((category) => {
-    categoriesList.push({
-      name: category,
-      posts: posts.filter((post) => post.data.category === category).length,
+    posts.map((post) => {
+      if ('category' in post.data) {
+        categoriesNames.add(post.data.category);
+      }
     });
-  });
-  const categoriesAndPosts = categoriesList.sort((a, b) => b.posts - a.posts);
 
-  return categoriesAndPosts;
+    [...categoriesNames].map((category) => {
+      categoriesList.push({
+        name: category,
+        url: `/${config.getFilter('slugify')(category)}/`,
+        posts: posts.filter((post) => post.data.category === category).length,
+      });
+    });
+
+    return categoriesList;
+  };
 };
 
 module.exports = categoriesList;
