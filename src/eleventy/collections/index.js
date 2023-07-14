c8933deb0ac9d70763c217b3cfc6a0fd7b486158
jucegt <julio2201@gmail.com>
2023-07-13 19:51:13 -0600

    feat: added pages collection

--- src/eleventy/collections/index.js
+++ src/eleventy/collections/index.js
@@ -4,6 +4,7 @@ const categoryByName = require('./category-by-name');
 const categoriesList = require('./categories-list');
 const pagedPosts = require('./paged-posts');
 const pagedCategories = require('./paged-categories');
+const pages = require('./pages');
 
 const collections = function (config) {
   config.addCollection('posts', posts);
@@ -12,6 +13,7 @@ const collections = function (config) {
   config.addCollection('categoriesList', categoriesList);
   config.addCollection('pagedPosts', pagedPosts);
   config.addCollection('pagedCategories', pagedCategories);
+  config.addCollection('pages', pages);
 };
 
 module.exports = collections;
