676a08d0b8acb9681acc71026db2c87c1480b152
jucegt <julio2201@gmail.com>
2023-07-18 15:21:47 -0600

    feat: amp plugins defer

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -27,11 +27,10 @@ const base = {
         <link rel="preload" as="font" href="/static/fonts/lato-bold.woff2" type="font/woff2" crossorigin>
         <link rel="preload" as="image" href="/static/webp/md/11ty.webp" imagesrcset="/static/webp/md/11ty.webp 600w, /static/webp/lg/11ty.webp 1200w" />
         <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
-        <link rel="preload" as="script" href="https://cdn.ampproject.org/v0/amp-form-0.1.js">
         <!-- AMP -->
         <script async src="https://cdn.ampproject.org/v0.js"></script>
-        <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
-        <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
+        <script defer custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
+        <script defer custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
         <!-- STYLES -->
         <style amp-custom>${await inlineCss('style.css')}</style>
         <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
