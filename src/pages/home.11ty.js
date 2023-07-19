6aa96bd87ec4f29dd065884f7bbef0ed942cf344
jucegt <julio2201@gmail.com>
2023-07-18 21:24:29 -0600

    feat: added pagination for home

--- src/pages/home.11ty.js
+++ src/pages/home.11ty.js
@@ -1,11 +1,31 @@
 const home = {
-  data: {
-    layout: 'base',
-    permalink: '/',
+  data: async function () {
+    return {
+      layout: 'base',
+      pagination: {
+        data: 'collections.posts',
+        size: 12,
+        alias: 'posts',
+      },
+      permalink: ({ pagination }) => {
+        let link = '/';
+        if (pagination.pageNumber > 0) {
+          link += `${pagination.pageNumber + 1}/`;
+        }
+        return link;
+      },
+    };
   },
-  render: function ({ config }) {
+  render: function ({ config, posts }) {
     return /* html */ `
       <div class="container">
+        ${posts
+          .map(
+            (post) => /* html */ `
+            <p>${post.url}</p>
+          `,
+          )
+          .join('')}
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong>Asperiores beatae</strong> neque facere temporibus, fugit ullam sequi quidem expedita aspernatur, assumenda aliquid modi perferendis enim nostrum architecto doloremque explicabo cupiditate voluptates!</p>
         <p>${config.language}</p>
         <p><img src="/11ty.png" alt="11ty" /></p>
