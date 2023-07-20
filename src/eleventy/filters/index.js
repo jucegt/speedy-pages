93d7ab13151ba1348def0b097a983f8f40f7efc2
jucegt <julio2201@gmail.com>
2023-07-20 00:13:29 -0600

    feat: added srcSet filter

--- src/eleventy/filters/index.js
+++ src/eleventy/filters/index.js
@@ -1,11 +1,13 @@
 const date = require('./date');
 const parseHTML = require('./parse-html');
 const src = require('./src');
+const srcSet = require('./src-set');
 
 const filters = function (config) {
   config.addFilter('date', date);
   config.addFilter('parseHTML', parseHTML);
   config.addFilter('src', src);
+  config.addFilter('srcSet', srcSet);
 };
 
 module.exports = filters;
