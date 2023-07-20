8bc179961e2dd63db013bd9b27146b9e4ab7695a
jucegt <julio2201@gmail.com>
2023-07-20 00:12:50 -0600

    feat: added src filter

--- src/eleventy/filters/index.js
+++ src/eleventy/filters/index.js
@@ -1,9 +1,11 @@
 const date = require('./date');
 const parseHTML = require('./parse-html');
+const src = require('./src');
 
 const filters = function (config) {
   config.addFilter('date', date);
   config.addFilter('parseHTML', parseHTML);
+  config.addFilter('src', src);
 };
 
 module.exports = filters;
