631c6e344038ccf0dee12df7ae029f4a6318b88f
jucegt <julio2201@gmail.com>
2023-07-20 00:11:59 -0600

    feat: added parseHTML filter

--- src/eleventy/filters/index.js
+++ src/eleventy/filters/index.js
@@ -1,7 +1,9 @@
 const date = require('./date');
+const parseHTML = require('./parse-html');
 
 const filters = function (config) {
   config.addFilter('date', date);
+  config.addFilter('parseHTML', parseHTML);
 };
 
 module.exports = filters;
