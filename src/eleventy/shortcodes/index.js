2c3f167bf3c0936d7832ccd5687e10a5f066530b
jucegt <julio2201@gmail.com>
2023-07-19 20:42:28 -0600

    feat: moved mask and state to shortcodes

--- src/eleventy/shortcodes/index.js
+++ src/eleventy/shortcodes/index.js
@@ -1,11 +1,15 @@
 const header = require('./header');
 const footer = require('./footer');
 const socialLinks = require('./social-links');
+const mask = require('./mask');
+const state = require('./state');
 
 const shortcodes = function (config) {
   config.addShortcode('header', header);
   config.addShortcode('footer', footer);
   config.addShortcode('socialLinks', socialLinks);
+  config.addShortcode('mask', mask);
+  config.addShortcode('state', state);
 };
 
 module.exports = shortcodes;
