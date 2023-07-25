a99b4d604eb0f99dbe8143545bce9657ef600780
jucegt <julio2201@gmail.com>
2023-07-24 18:23:42 -0600

    feat: added markdown it filter

--- src/eleventy/filters/index.js
+++ src/eleventy/filters/index.js
@@ -2,12 +2,14 @@ const date = require('./date');
 const parseHTML = require('./parse-html');
 const src = require('./src');
 const srcSet = require('./src-set');
+const markdown = require('./markdown-it');
 
 const filters = function (config) {
   config.addFilter('date', date);
   config.addFilter('parseHTML', parseHTML);
   config.addFilter('src', src);
   config.addFilter('srcSet', srcSet);
+  config.addFilter('markdown', markdown);
 };
 
 module.exports = filters;
