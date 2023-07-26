4e3c6c6cbd64a929f94c76334d05a11e29725100
jucegt <julio2201@gmail.com>
2023-07-26 00:19:57 -0600

    feat: added locale for decap cms

---
+++ src/data/locale/en.js
@@ -0,0 +1,136 @@
+const en = {
+  posts: {
+    singular: 'Post',
+    plural: 'Posts',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in Google search results.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Link structure displayed in the navigation bar.',
+      },
+      description: {
+        label: 'Description',
+        error: 'The description should be between 140 and 160 characters',
+        hint: 'Description displayed in Google search results.',
+      },
+    },
+    info: {
+      label: 'Information',
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in the post page.',
+      },
+      category: {
+        label: 'Category',
+        hint: 'Category displayed in the post page.',
+      },
+      date: {
+        label: 'Publish Date',
+        hint: 'Date displayed in the post page.',
+      },
+      author: {
+        label: 'Author',
+        hint: 'Name displayed in the post page.',
+      },
+      excerpt: {
+        label: 'Excerpt',
+        hint: 'Excerpt displayed in the post page.',
+      },
+    },
+    content: {
+      label: 'Content',
+      heroImage: {
+        label: 'Hero Image',
+        hint: 'Image displayed in the post page.',
+      },
+      body: {
+        label: 'Body',
+        hint: 'Content displayed in the post page.',
+      },
+    },
+  },
+  categories: {
+    singular: 'Category',
+    plural: 'Categories',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in Google search results.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Link structure displayed in the navigation bar.',
+      },
+      description: {
+        label: 'Description',
+        error: 'The description should be between 140 and 160 characters',
+        hint: 'Description displayed in Google search results.',
+      },
+    },
+    info: {
+      label: 'Information',
+      showMenu: {
+        label: 'Show in menu',
+        hint: 'Show category in the navigation bar.',
+      },
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in the category page.',
+      },
+      body: {
+        label: 'Body',
+        hint: 'Content displayed in the category page.',
+      },
+    },
+  },
+  authors: {
+    singular: 'Author',
+    plural: 'Authors',
+    title: {
+      label: 'Name',
+      hint: 'Name displayed in the post page.',
+    },
+    photo: {
+      label: 'Photo',
+      hint: 'Photo displayed in the post page.',
+    },
+  },
+  pages: {
+    singular: 'Page',
+    plural: 'Pages',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in Google search results.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Link structure displayed in the navigation bar.',
+      },
+      description: {
+        label: 'Description',
+        error: 'The description should be between 140 and 160 characters',
+        hint: 'Description displayed in Google search results.',
+      },
+    },
+    content: {
+      label: 'Content',
+      title: {
+        label: 'Title',
+        hint: 'Title displayed in the page.',
+      },
+      body: {
+        label: 'Body',
+        hint: 'Content displayed in the page.',
+      },
+    },
+  },
+};
+
+module.exports = en;
---
+++ src/data/locale/es.js
@@ -0,0 +1,136 @@
+const es = {
+  posts: {
+    singular: 'Artículo',
+    plural: 'Artículos',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en los resultados de búsqueda de Google.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Estructura de enlace que se muestra en la barra de navegación.',
+      },
+      description: {
+        label: 'Descripción',
+        error: 'La descripción debe tener entre 140 y 160 caracteres',
+        hint: 'Descripción que se muestra en los resultados de búsqueda de Google.',
+      },
+    },
+    info: {
+      label: 'Información',
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en la página del artículo.',
+      },
+      category: {
+        label: 'Categoría',
+        hint: 'Categoría que se muestra en la página del artículo.',
+      },
+      date: {
+        label: 'Fecha de publicación',
+        hint: 'Fecha que se muestra en la página del artículo.',
+      },
+      author: {
+        label: 'Autor',
+        hint: 'Nombre que se muestra en la página del artículo.',
+      },
+      excerpt: {
+        label: 'Extracto',
+        hint: 'Extracto que se muestra en la página del artículo.',
+      },
+    },
+    content: {
+      label: 'Contenido',
+      heroImage: {
+        label: 'Imagen de portada',
+        hint: 'Imagen que se muestra en la página del artículo.',
+      },
+      body: {
+        label: 'Cuerpo',
+        hint: 'Contenido que se muestra en la página del artículo.',
+      },
+    },
+  },
+  categories: {
+    singular: 'Categoría',
+    plural: 'Categorías',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en los resultados de búsqueda de Google.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Estructura de enlace que se muestra en la barra de navegación.',
+      },
+      description: {
+        label: 'Descripción',
+        error: 'La descripción debe tener entre 140 y 160 caracteres',
+        hint: 'Descripción que se muestra en los resultados de búsqueda de Google.',
+      },
+    },
+    info: {
+      label: 'Información',
+      showMenu: {
+        label: 'Mostrar en el menú',
+        hint: 'Mostrar en el menú de navegación.',
+      },
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en la página de la categoría.',
+      },
+      body: {
+        label: 'Cuerpo',
+        hint: 'Contenido que se muestra en la página de la categoría.',
+      },
+    },
+  },
+  authors: {
+    singular: 'Autor',
+    plural: 'Autores',
+    title: {
+      label: 'Nombre',
+      hint: 'Nombre que se muestra en los artículos.',
+    },
+    photo: {
+      label: 'Foto',
+      hint: 'Foto que se muestra en los artículos.',
+    },
+  },
+  pages: {
+    singular: 'Página',
+    plural: 'Páginas',
+    seo: {
+      label: 'SEO',
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en los resultados de búsqueda de Google.',
+      },
+      slug: {
+        label: 'Slug',
+        hint: 'Estructura de enlace que se muestra en la barra de navegación.',
+      },
+      description: {
+        label: 'Descripción',
+        error: 'La descripción debe tener entre 140 y 160 caracteres',
+        hint: 'Descripción que se muestra en los resultados de búsqueda de Google.',
+      },
+    },
+    content: {
+      label: 'Contenido',
+      title: {
+        label: 'Título',
+        hint: 'Título que se muestra en la página.',
+      },
+      body: {
+        label: 'Cuerpo',
+        hint: 'Contenido que se muestra en la página.',
+      },
+    },
+  },
+};
+
+module.exports = es;
