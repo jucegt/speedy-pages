ee8881b6f8ce9a39223f59be960fc70393c329f7
jucegt <julio2201@gmail.com>
2023-08-02 00:28:05 -0600

    feat: added basic structure for post

--- src/layouts/post.11ty.js
+++ src/layouts/post.11ty.js
@@ -1,12 +1,34 @@
+const getReadingTime = require('../utilities/reading-time');
+
 const post = {
   data: {
     layout: 'base',
   },
-  render: function ({ content }) {
+  render: function ({ content, info, image }) {
+    const readingTime = getReadingTime(content);
     return /* html */ `
-      <div class="container post">
-        ${content}
-      </div>
+      <p><a href="/${info.category}/">${info.category}</a></p>
+      <h1>${info.title}</h1>
+      <p>${info.author} - ${this.date(info.date)} - ${readingTime} minutos de lectura</p>
+      <p>${info.excerpt}</p>
+      <amp-img
+        alt="${info.title}"
+        src="${this.src(image, true)}"
+        srcset="${this.srcSet(image, true)}"
+        width="400"
+        height="300"
+        layout="responsive">
+        <amp-img
+          fallback
+          alt="${info.title}"
+          src="${this.src(image)}"
+          srcset="${this.srcSet(image)}"
+          width="400"
+          height="300"
+          layout="responsive">
+        </amp-img>
+      </amp-img>
+      ${content}
     `;
   },
 };
