53f6f2109dca7ac9eb748c2a5800d80440b77b14
jucegt <julio2201@gmail.com>
2023-08-03 17:25:59 -0600

    feat: added fuse for search api

--- src/api/search.js
+++ src/api/search.js
@@ -1,11 +1,24 @@
+import Fuse from 'fuse.js';
 import data from '../../.11ty/search/info.json';
 
-export const handler = async () => {
+export const handler = async ({ queryStringParameters }) => {
+  const { q } = queryStringParameters;
+  if (!q) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: true, message: 'Missing search parameter' }),
+    };
+  }
+  const fuse = new Fuse(data.pages, {
+    keys: ['title', 'heading', 'description'],
+  });
+  const results = fuse.search(q);
+
   return {
     statusCode: 200,
     headers: {
       'content-type': 'application/json',
     },
-    body: JSON.stringify({ results: data.pages }),
+    body: JSON.stringify({ results }),
   };
 };
