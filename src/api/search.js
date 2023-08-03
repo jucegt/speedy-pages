3584a462ca376bf5650f12b184d3accf4fcd725c
jucegt <julio2201@gmail.com>
2023-08-03 00:50:31 -0600

    feat: fixed event

--- src/api/search.js
+++ src/api/search.js
@@ -1,8 +1,6 @@
-const handler = async () => {
+export const handler = async () => {
   return {
     status: 200,
     body: JSON.stringify({ message: 'Search API' }),
   };
 };
-
-module.exports = { handler };
