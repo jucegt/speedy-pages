f49f8aaf006df163c9d41aa29f565004e2cfdf9d
jucegt <julio2201@gmail.com>
2023-08-02 21:16:22 -0600

    feat: added post styles

--- src/layouts/post.11ty.js
+++ src/layouts/post.11ty.js
@@ -1,34 +1,63 @@
 const getReadingTime = require('../utilities/reading-time');
+const calendar = require('../svgs/calendar');
+const clock = require('../svgs/clock');
 
 const post = {
   data: {
     layout: 'base',
   },
-  render: function ({ content, info, image }) {
+  render: function ({ content, info, image, collections }) {
+    const { authors } = collections;
+    const author = authors.find((author) => author.data.title === info.author);
     const readingTime = getReadingTime(content);
     return /* html */ `
-      <p><a href="/${info.category}/">${info.category}</a></p>
-      <h1>${info.title}</h1>
-      <p>${info.author} - ${this.date(info.date)} - ${readingTime} minutos de lectura</p>
-      <p>${info.excerpt}</p>
-      <amp-img
-        alt="${info.title}"
-        src="${this.src(image, true)}"
-        srcset="${this.srcSet(image, true)}"
-        width="400"
-        height="300"
-        layout="responsive">
-        <amp-img
-          fallback
-          alt="${info.title}"
-          src="${this.src(image)}"
-          srcset="${this.srcSet(image)}"
-          width="400"
-          height="300"
-          layout="responsive">
-        </amp-img>
-      </amp-img>
-      ${content}
+      <article class="post">
+        <section class="post__header">
+          <a class="post__category" href="/${info.category}/">${info.category}</a>
+          <h1 class="post__title">${info.title}</h1>
+          <p class="post__info">
+            <span>
+              <amp-img
+                alt="${info.author}"
+                src="${this.photo(author.data.photo)}"
+                width="24"
+                height="24"
+                layout="responsive">
+              </amp-img>
+              ${info.author}
+            </span>
+            <span>${calendar()} ${this.date(info.date)}</span>
+            <span>${clock()} ${readingTime} minutos de lectura</span>
+          </p>
+          <p class="post__excerpt">${info.excerpt}</p>
+          <amp-img
+            class="post__image"
+            alt="${info.title}"
+            src="${this.src(image, true)}"
+            srcset="${this.srcSet(image, true)}"
+            width="400"
+            height="300"
+            layout="responsive">
+            <amp-img
+              fallback
+              alt="${info.title}"
+              src="${this.src(image)}"
+              srcset="${this.srcSet(image)}"
+              width="400"
+              height="300"
+              layout="responsive">
+            </amp-img>
+          </amp-img>
+        </section>
+        <section class="post__body">
+          <div class="post__content">
+            ${content}
+          </div>
+          <div class="post__ads">
+            Anuncios
+          </div>
+        </section>
+      </article>
     `;
   },
 };
