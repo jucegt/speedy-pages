fdf4e1bccae7121e59bcf383aa61bb840076685f
jucegt <julio2201@gmail.com>
2023-08-03 17:17:04 -0600

    feat: added content type for search api

--- src/api/search.js
+++ src/api/search.js
@@ -3,6 +3,9 @@ import data from '../../.11ty/search/info.json';
 export const handler = async () => {
   return {
     statusCode: 200,
+    headers: {
+      'content-type': 'application/json',
+    },
     body: JSON.stringify({ results: data.pages }),
   };
 };
