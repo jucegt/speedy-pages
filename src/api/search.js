2834ed7628fa3c335f6903730104d0f32b17a26e
jucegt <julio2201@gmail.com>
2023-08-03 00:53:38 -0600

    feat: added search results

--- src/api/search.js
+++ src/api/search.js
@@ -1,6 +1,8 @@
+import data from '../../.11ty/search/info.json';
+
 export const handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Search API' }),
+    body: JSON.stringify({ results: data }),
   };
 };
