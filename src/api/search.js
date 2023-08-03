d46f00be6be4d89848a8a82b93783c3a6659327d
jucegt <julio2201@gmail.com>
2023-08-03 00:55:50 -0600

    feat: return pages

--- src/api/search.js
+++ src/api/search.js
@@ -3,6 +3,6 @@ import data from '../../.11ty/search/info.json';
 export const handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ results: data }),
+    body: JSON.stringify({ results: data.pages }),
   };
 };
