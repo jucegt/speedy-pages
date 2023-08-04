49491f04694c5b562bf8a82ff7e71bb36e44ab4d
jucegt <julio2201@gmail.com>
2023-08-03 18:30:32 -0600

    feat: test search results

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -52,6 +52,8 @@ const base = {
         <script async src="https://cdn.ampproject.org/v0.js"></script>
         <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
         <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
+        <script async custom-element="amp-list" src="https://cdn.ampproject.org/v0/amp-list-0.1.js"></script>
+        <script async custom-template="amp-mustache" src="https://cdn.ampproject.org/v0/amp-mustache-0.2.js"></script>
         <!-- STYLES -->
         <style amp-custom>${await inlineCss('style.css')}</style>
         <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
@@ -60,6 +62,17 @@ const base = {
       </head>
       <body>
         ${this.header(seo, multicolor.code, navigation, url)}
+        <div class="search">
+          <amp-list
+            layout="responsive"
+            [src]="query ? 'https://speedypages.netlify.app/api/search?q=' + encodeURIComponent(query) : null">
+            <template type="amp-mustache">
+              <div>
+                <a href="{{item.url}}">{{item.title}}</a>
+              </div>
+            </template>
+          </amp-list>
+        </div>
         <main class="content container">
           ${content}
         </main>
