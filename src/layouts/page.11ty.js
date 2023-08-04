a8afe240ab56c101135c0fbd69d2ba06c17b1b86
jucegt <julio2201@gmail.com>
2023-08-03 23:42:04 -0600

    feat: added page styles

--- src/layouts/page.11ty.js
+++ src/layouts/page.11ty.js
@@ -2,9 +2,18 @@ const page = {
   data: {
     layout: 'base',
   },
-  render: function ({ content }) {
+  render: function ({ content, seo }) {
     return /* html */ `
-      ${content}
+      <article class="post">
+        <section class="post__header">
+          <h1 class="post__title">${seo.heading}</h1>
+        </section>
+        <section class="post__body">
+          <div class="post__content">
+            ${content}
+          </div>
+        </section>
+      </article>
     `;
   },
 };
