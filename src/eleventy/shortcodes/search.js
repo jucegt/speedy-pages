fc0589eba5340ecd72020adc36bf4fd17c1bf4dd
jucegt <julio2201@gmail.com>
2023-08-03 23:08:04 -0600

    feat: added styles for search desktop

--- src/eleventy/shortcodes/search.js
+++ src/eleventy/shortcodes/search.js
@@ -18,25 +18,6 @@ const search = function () {
           <template type="amp-mustache">
             <article class="search-box__article">
               <header class="article__header">
-                {{#item.image}}
-                  <amp-img
-                    alt="{{item.title}}"
-                    src="{item.image.webp.src}"
-                    srcset="{{item.image.webp.srcSet}}"
-                    width="600"
-                    height="480"
-                    layout="responsive">
-                    <amp-img
-                      fallback
-                      alt="{{item.title}}"
-                      src="{{item.image.default.src}}"
-                      srcset="{{item.image.default.srcSet}}"
-                      width="600"
-                      height="480"
-                      layout="responsive">
-                    </amp-img>
-                  </amp-img>
-                {{/item.image}}
                 <p><a href="{{item.url}}">{{item.title}}</a></p>
               </header>
               <section class="article__content">
